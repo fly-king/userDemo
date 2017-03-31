678c3c30e6479bd8856e05fb6c0d40a8a38a219c
tianda.lt <tianda.lt@alibaba-inc.com>
2017-03-31 20:23:21 +0800

    spring配置文件移动到resource文件夹下

---
+++ src/main/java/com/alibaba/taobao/userDemo/model/User.java
@@ -0,0 +1,26 @@
+package com.alibaba.taobao.userdemo.model;
+
+/**
+ * Created by tianda.lt on 2017/3/31.
+ */
+public class User {
+
+    private String NAME;
+    private String age;
+
+    public String getNAME() {
+        return NAME;
+    }
+
+    public void setNAME(String NAME) {
+        this.NAME = NAME;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
